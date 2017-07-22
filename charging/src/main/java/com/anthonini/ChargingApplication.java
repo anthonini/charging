95a95dd0c0b507b351593832d06d1c131c1d62c3
Anthonini <anthonini@gmail.com>
2017-07-22 19:24:55 -0300

    #closing 1.0.0 version
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- charging/src/main/java/com/anthonini/ChargingApplication.java
+++ charging/src/main/java/com/anthonini/ChargingApplication.java
@@ -4,12 +4,16 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-public class ChargingApplication {
+public class ChargingApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChargingApplication.class, args);
@@ -18,5 +22,14 @@ public class ChargingApplication {
 	@Bean
 	public LocaleResolver localResolver(){
 		return new FixedLocaleResolver(new Locale("pt","BR"));
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/bankslip");
+		}
 	}
 }
